b7529a23d3fb4c6f5068eed5fabaff7a2eb701d2
Tony CoderYo <abundantant@gmail.com>
2019-02-05 00:15:52 -0500

    modified files

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
 // set up routes
 app.use(routes);
 
