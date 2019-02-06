e4393405298a6e35e9b7e886cde5e92537399f7c
Tony CoderYo <abundantant@gmail.com>
2019-02-05 21:25:59 -0500

    modified files

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -1,16 +1,12 @@
 const router = require("express").Router();
 const path = require("path");
 
-router
-  .route("/")
-  .get(function(req, res) {
+router.get("/", function(req, res) {
    // SEND HOME PAGE USING RES.SENDFILE AND PATH.JOIN()
-   res.sendFile(path.join(__dirname, "../../public/home.html"));
+     res.sendFile(path.join(__dirname, "../../public/home.html"));
   });
 
-router
-  .route("/notes")
-  .get(function (req, res) {
+router.get("/notes", function (req, res) {
     // SEND NOTES PAGE USING RES.SENDFILE AND PATH.JOIN()
     res.sendFile(path.join(__dirname, "../../public/notes.html"));
   });
