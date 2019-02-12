f19bc1e096941d3e0ab5cd857f573948240e8999
Tony CoderYo <abundantant@gmail.com>
2019-02-12 10:16:02 -0500

    modified files

--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 
+  renderNotes();
+
   $("#save-note").on("click", function (e) {
-    e.preventDefault();
 
     // package up data from form
     const newNote = {
@@ -15,22 +16,33 @@ $(document).ready(function () {
       data: newNote // this is req.body
     }).then(function (dbResponse) {
       console.log(dbResponse);
-      location.reload();
+      renderNotes();
     });
   })
 
-  // $.ajax({
-  //   url: "/api/notes",
-  //   method: "GET",
-  // }).then(function (dbResponse) {
-  //   console.log(dbResponse);
-  //   // location.reload();
-  // });
+  function renderNotes() {
 
+    $.ajax({
+      url: "api/notes",
+      method: "GET"
+    }).then(function (results) {
 
+      for (let i = 0; i < results.length; i++) {
 
-
-
-
-
+        const dbData =
+                  `<div class="card">
+                    <div class="card-body" id=${results[i].id}>
+                      <h5 class="card-subtitle mb-2 text-muted" id="newNote-title">${results[i].title}</h5>
+                      <p class="card-text" id="newNote-body">
+                        ${results[i].body}
+                        <a><i class="fas fa-trash-alt deleteNote"></i></a>
+                        <a><i class="fas fa-edit editNote"></i></a>
+                      </p>
+                    </div>
+                  </div>`
+        $("#notesDiv").prepend(dbData);
+      }
+    })
+  }
 })
+
